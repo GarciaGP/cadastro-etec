342807c113a1ea2cd1fe2434b3fbb5bef4116025
Gabriel <gabriel.garp@outlook.com>
2019-05-03 20:23:08 -0300

    Início do projeto

---
+++ src/main/java/br/com/rafaelgabriel/cadastroEtec/CadastroEtecApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelgabriel.cadastroEtec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroEtecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroEtecApplication.class, args);
+	}
+
+}
