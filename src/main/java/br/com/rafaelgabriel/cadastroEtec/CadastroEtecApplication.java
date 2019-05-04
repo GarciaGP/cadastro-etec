f5345aa6d81b0950cd2157269b2adee5ae9976f4
Gabriel <gabriel.garp@outlook.com>
2019-05-03 21:37:01 -0300

    Update das configurações do projeto e adição do primeiro menu xhtml

--- src/main/java/br/com/rafaelgabriel/cadastroEtec/CadastroEtecApplication.java
+++ src/main/java/br/com/rafaelgabriel/cadastroEtec/CadastroEtecApplication.java
@@ -1,13 +1,22 @@
 package br.com.rafaelgabriel.cadastroEtec;
 
+import javax.faces.webapp.FacesServlet;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CadastroEtecApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroEtecApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean<FacesServlet> servletRegistrationBean() {
+		return new ServletRegistrationBean<FacesServlet>(new FacesServlet(), "*.xhtml");
 	}
 
 }
